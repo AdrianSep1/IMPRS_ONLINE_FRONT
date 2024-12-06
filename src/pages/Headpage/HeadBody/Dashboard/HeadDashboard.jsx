247e815afe4af2378de68189ead09921900c71f9
bydear18 <136951610+bydear18@users.noreply.github.com>
2024-12-06 10:21:21 +0800

    Update HeadDashboard.jsx

--- src/pages/Headpage/HeadBody/Dashboard/HeadDashboard.jsx
+++ src/pages/Headpage/HeadBody/Dashboard/HeadDashboard.jsx
@@ -54,7 +54,7 @@ const Dashboard = () => {
   const [userID, setUserID] = useState('');
   const [schoolId, setSchoolId]= useState('');
   const [comments, setComments] = useState([]);
-  const [college, setCollege] = useState();
+  const [content, setContent] = useState([]);
   const [requesterName, setRequesterName] = useState('');
   const [requesterEmail, setRequesterEmail] = useState('');
   const [contactNumber, setContactNumber] = useState('');
@@ -81,7 +81,7 @@ const Dashboard = () => {
   });
 
   useEffect(() => {
-    fetch("https://imprsonlineback-production.up.railway.app/records/requestCounts")
+    fetch("http://localhost:8080/records/requestCounts")
       .then((response) => response.json())
       .then((data) => {
         setStatistics((prevState) => ({
@@ -113,11 +113,10 @@ const Dashboard = () => {
       },
     };
 
-    fetch("https://imprsonlineback-production.up.railway.app/requests/all", requestOptions)
+    fetch("http://localhost:8080/requests/all", requestOptions)
       .then((response) => response.json())
       .then((data) => {
         setValues(data);
-        setCollege(data['college']);
       })
       .catch(error => {
         console.log(error);
@@ -150,7 +149,7 @@ const Dashboard = () => {
     };
   
     // Fetch all records from the backend
-    fetch(`https://imprsonlineback-production.up.railway.app/records/all`, requestOptions)
+    fetch(`http://localhost:8080/requests/all`, requestOptions)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -186,9 +185,7 @@ const closeModal = () => {
     setShow('hide');
 }
 
-
 const getCustomSeverityClass = (status) => {
-    
   switch (status) {
       case 'Ready to Claim':
           return 'custom-completed';
@@ -207,7 +204,7 @@ const getCustomSeverityClass = (status) => {
 
 const renderSeverityTag = (rowData) => {
   if (rowData.status === 'Rejected') {
-      rowData.status = 'Rejected';
+    rowData.status = 'Rejected';
   } else if (rowData.status === 'Pending') {
       rowData.status = 'Waiting for Approval';
   } else if (rowData.status === 'In Progress') {
@@ -217,7 +214,7 @@ const renderSeverityTag = (rowData) => {
   } else if (rowData.status=== 'Claimed') {
       rowData.status = 'Ready to Claim';
   } 
-  
+
   const severityClass = getCustomSeverityClass(rowData.status);
   return <span className={severityClass}>{rowData.status}</span>;
 };
@@ -284,55 +281,54 @@ const renderSeverityTag = (rowData) => {
         </div>
 
         <div id="dashboardTable">
-          <DataTable 
-              value={values} 
-              scrollable 
-              scrollHeight="20vw" 
-              header={header} 
-              globalFilterFields={['requesterName', 'college', 'department', 'requestID', 'description', 'colored', 'fileType', 'fileName', 'paperSize', 'paperType', 'requestDate', 'requestTime', 'role']}
-              filters={filters} 
-              emptyMessage="No records found."
-              paginator 
-              rows={8}
-              tableStyle={{ minWidth: '20vw' }} 
-              selectionMode="single"
-          >
-              <Column field="requesterName" header="Requester's Name" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              
-              {/* Custom rendering logic for the College column */}
-              <Column 
-                  field="college" 
-                  header="College" 
-                  sortable 
-                  headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}
-                  body={(rowData) => (
-                      rowData.college === "Elementary" || 
-                      rowData.college === "Junior High School" || 
-                      rowData.college === "Senior High School" ||
-                      rowData.college === "University Library" ||
-                      rowData.college === "Vice President for Academic Affairs" 
-                          ? "" // Show empty if it's one of the specified levels
-                          : rowData.college // Show actual college value otherwise
-                  )}
-              ></Column>
-              
-              <Column field="department" header="Department" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="requestID" header="Request ID" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="fileType" header="File Type" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="fileName" header="File Name" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="description" header="Description" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="noOfCopies" header="No. of Copies" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="colored" header="Colored" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="paperSize" header="Paper Size" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="paperType" header="Paper Type" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="requestDate" header="Request Date" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="requestTime" header="Request Time" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="useDate" header="Use Date" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="role" header="Role" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
-              <Column field="status" header="Status" body={renderSeverityTag} headerStyle={{ width: '150px', paddingRight: '150px', whiteSpace: 'nowrap' }}></Column>
-          </DataTable>
-      </div>
-
+              <DataTable 
+                value={values} 
+                scrollable 
+                scrollHeight="20vw" 
+                header={header} 
+                globalFilterFields={['requesterName', 'college', 'department', 'requestID', 'description', 'colored', 'fileType', 'fileName', 'paperSize', 'paperType', 'requestDate', 'requestTime', 'role']}
+                filters={filters} 
+                emptyMessage="No records found."
+                paginator 
+                rows={8}
+                tableStyle={{ minWidth: '20vw' }} 
+                selectionMode="single"
+                >
+                    <Column field="requesterName" header="Requester's Name" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    
+                    {/* Custom rendering logic for the College column */}
+                    <Column 
+                        field="college" 
+                        header="College" 
+                        sortable 
+                        headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}
+                        body={(rowData) => (
+                            rowData.college === "Elementary" || 
+                            rowData.college === "Junior High School" || 
+                            rowData.college === "Senior High School" ||
+                            rowData.college === "University Library" ||
+                            rowData.college === "Vice President for Academic Affairs" 
+                                ? "" // Show empty if it's one of the specified levels
+                                : rowData.college // Show actual college value otherwise
+                        )}
+                    ></Column>
+                    
+                    <Column field="department" header="Department" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="requestID" header="Request ID" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="fileType" header="File Type" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="fileName" header="File Name" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="description" header="Description" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="noOfCopies" header="No. of Copies" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="colored" header="Colored" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="paperSize" header="Paper Size" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="paperType" header="Paper Type" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="requestDate" header="Request Date" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="requestTime" header="Request Time" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="useDate" header="Use Date" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="role" header="Role" sortable headerStyle={{ width: '150px', whiteSpace: 'nowrap' }}></Column>
+                    <Column field="status" header="Status" body={renderSeverityTag} headerStyle={{ width: '150px', paddingRight: '150px', whiteSpace: 'nowrap' }}></Column>
+                </DataTable>
+        </div>
       </div>
     </div>
   );
